e8b1f11a78570598f86b8d93d5dcc703c79174cb
Mario Roberto <mrgomez.dev@gmail.com>
2025-04-21 16:45:08 -0600

    Uso de Outputs en Angular

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,7 +9,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
-  @Input({required: true}) img: string = '';
-  @Input({required: true}) price: number = 0;
-  @Input({required: true}) title: string = '';
+  @Input() img: string = '';
+  @Input() price: number = 0;
+  @Input() title: string = '';
+
+  @Output() addToCart = new EventEmitter();
+
+  addToCartHandler() {
+    console.log('click from child');
+    this.addToCart.emit({
+      title: this.title,
+      price: this.price,
+      img: this.img
+    });
+  }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -10,5 +10,13 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrl: './list.component.css'
 })
 export class ListComponent {
+  // Código original
+  // fromChild(event: any) {
+  //   console.log(event);
+  // }
 
+  // Código corregido
+  onAddToShoppingCart(product: any) {
+    console.log('Product added to cart:', product);
+  }
 }
